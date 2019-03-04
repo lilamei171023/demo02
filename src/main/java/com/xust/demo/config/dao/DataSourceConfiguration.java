be239f7474a9ecfc69f78780890e1189838ac887
lilamei02 <lilamei02@meituan.com>
2019-03-04 14:55:33 +0800

    change something

--- src/main/java/com/xust/demo/config/dao/DataSourceConfiguration.java
+++ src/main/java/com/xust/demo/config/dao/DataSourceConfiguration.java
@@ -4,7 +4,10 @@ package com.xust.demo.config.dao;
  * Create by Rowena on 2018/7/26
  */
 public class DataSourceConfiguration {
-    public void testcreateDateSource(){
-        System.out.println("the second changing");
+
+
+    public void createDateSource() {
+        System.out.println("change something!");
+
     }
 }
