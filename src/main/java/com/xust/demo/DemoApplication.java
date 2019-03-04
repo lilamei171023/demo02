743c235a6b1664950009cfd7a1dfb02083fddb5a
lilamei02 <lilamei02@meituan.com>
2019-03-04 19:33:41 +0800

    test3

--- src/main/java/com/xust/demo/DemoApplication.java
+++ src/main/java/com/xust/demo/DemoApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 	// test 2
 	public static void main(String[] args) {
+		//test3
 		SpringApplication.run(DemoApplication.class, args);
 	}
 	//test1
