c41f6d67a198f63be58742e07fada972ced534d7
lilamei02 <lilamei02@meituan.com>
2019-03-04 14:55:33 +0800

    change something

--- src/main/java/com/xust/demo/config/dao/DataSourceConfiguration.java
+++ src/main/java/com/xust/demo/config/dao/DataSourceConfiguration.java
@@ -4,8 +4,7 @@ package com.xust.demo.config.dao;
  * Create by Rowena on 2018/7/26
  */
 public class DataSourceConfiguration {
-    public CombolPooledDateSOurce createDateSource(){
-      Combolpoo
-        return null;
+    public void createDateSource(){
+        System.out.println("change something!");
     }
 }
