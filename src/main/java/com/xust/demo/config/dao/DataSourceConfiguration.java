db0d2a04c5417b742ef8663bb388d2b361e6fa7a
lilamei02 <lilamei02@meituan.com>
2019-03-04 15:48:50 +0800

    dev2 change+dev2 entity

--- src/main/java/com/xust/demo/config/dao/DataSourceConfiguration.java
+++ src/main/java/com/xust/demo/config/dao/DataSourceConfiguration.java
@@ -7,6 +7,11 @@ public class DataSourceConfiguration {
 
 
     public void createDateSource(){
+<<<<<<< HEAD
         System.out.println("dev1 change something!");
+=======
+
+        System.out.println("dev2 change something!");
+>>>>>>> dev2 change+dev2 entity
     }
 }
---
+++ src/main/java/com/xust/demo/entity/dev2.java
@@ -0,0 +1,21 @@
+// Copyright (C) 2019 Meituan
+// All rights reserved
+package com.xust.demo.entity;
+
+import lombok.Data;
+
+/**
+ * @author rowena
+ * @version 1.0
+ * @created 2019-03-04 15:46
+ **/
+@Data
+public class dev2 {
+
+    private int  id;
+
+    private String name;
+
+    private String  number;
+
+}
